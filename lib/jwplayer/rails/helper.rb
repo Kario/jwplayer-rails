2559972067b1f0348bb817660758e9b067e5e5bc
Troll-Topper <cmoski@gmail.com>
2015-12-02 09:07:15 -0800

    Update helper.rb

--- lib/jwplayer/rails/helper.rb
+++ lib/jwplayer/rails/helper.rb
@@ -2,7 +2,7 @@ module JWPlayer::Rails
   module Helper
     DEFAULT_OPTIONS = {
       id: 'jwplayer',
-      flashplayer: '/assets/jwplayer.flash.swf'
+      flashplayer: asset_path('jwplayer.flash.swf')
     }
 
     LICENSE_OPTIONS = {
